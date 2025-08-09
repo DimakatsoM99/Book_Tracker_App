f95c0908930a31cfd38cce364dbee17fba40e4b3
Dimakatso <dimakatso.mokhomo@capaciti.org.za>
2025-08-09 18:32:27 +0200

    Added styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import BookList from './components/BookList';
 import BookForm from './components/BookForm';
+import BookList from './components/BookList';
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ maxWidth: 900, margin: '24px auto', fontFamily: 'Arial, sans-serif' }}>
       <h1>📚 Book Tracker</h1>
       <BookForm />
+      <hr />
       <BookList />
     </div>
   );
